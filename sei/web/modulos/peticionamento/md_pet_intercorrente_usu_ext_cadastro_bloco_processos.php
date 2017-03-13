dc60a6fcdc2e55da4ca3889d9d40aa73e88ba4ca
Nei Jobson <neijobson@anatel.gov.br>
2017-03-13 16:10:45 -0300

    Ajustes nos tamanhos dos campos Número e Tipo na tela do Peticionamento Intercorrent e retirada do Tabindex do campo do Tipo que é somente leitura.

--- sei/web/modulos/peticionamento/md_pet_intercorrente_usu_ext_cadastro_bloco_processos.php
+++ sei/web/modulos/peticionamento/md_pet_intercorrente_usu_ext_cadastro_bloco_processos.php
@@ -3,37 +3,22 @@
 <fieldset id="field_processos" class="infraFieldset sizeFieldset" xmlns="http://www.w3.org/1999/html">
 <legend class="infraLegend">&nbsp; Processo &nbsp;</legend>
 
-    <div class="bloco" style="width: 230px;">
-
-        <label id="lblNumeroSei" for="txtNumeroProcesso" accesskey="f" class="infraLabelObrigatorio">
-            Número:
-        </label>
-
-        <input onchange="controlarChangeNumeroProcesso();" type="text" id="txtNumeroProcesso" name="txtNumeroProcesso" class="infraText" maxlength="30" style="width:169px;" tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" value="<?= $txtNumeroProcesso ?>"/>
-
-        <button tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" type="button" accesskey="V" id="btnValidar" onclick="validarNumeroProcesso()" class="infraButton">
-            <span class="infraTeclaAtalho">V</span>alidar
-        </button>
+    <!-- INICIO NUMERO DO PROCESSO -->
+    <div class="bloco" style="width: 240px;">
+        <label id="lblNumeroSei" for="txtNumeroProcesso" accesskey="f" class="infraLabelObrigatorio">Número:</label>
+        <input onchange="controlarChangeNumeroProcesso();" type="text" id="txtNumeroProcesso" name="txtNumeroProcesso" class="infraText" maxlength="100" style="width: 184px;" tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" value="<?= $txtNumeroProcesso ?>"/>
+        <button tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" type="button" accesskey="V" id="btnValidar" onclick="validarNumeroProcesso()" class="infraButton"><span class="infraTeclaAtalho">V</span>alidar</button>
     </div>
-    <!--FIM NUMERO SEI-->
-
-    <!--TIPO-->
-    <div class="bloco" style="width: 340px;">
-    
-        <label id="lblTipo" for="txtTipo" accesskey="f" class="infraLabelObrigatorio">
-            Tipo:
-        </label>
-
-        <input type="text" id="txtTipo" name="txtTipo" class="infraText" readonly="readonly" tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" value="<?= $txtTipo ?>"/>
-
-        <button type="button" tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" onclick="adicionarProcesso();" accesskey="A" id="btnAdicionar" class="infraButton" style="display: none">
-            <span class="infraTeclaAtalho">A</span>dicionar
-        </button>
-
+    <!-- FIM NUMERO DO PROCESSO -->
+    <!-- INICIO TIPO DO PROCESSO VALIDADO -->
+    <div class="bloco" style="width: 380px;">
+        <label id="lblTipo" for="txtTipo" accesskey="f" class="infraLabelObrigatorio">Tipo:</label>
+        <input type="text" id="txtTipo" name="txtTipo" class="infraText" readonly="readonly" style="width: 318px;" value="<?= $txtTipo ?>"/>
+        <button type="button" tabindex="<?= PaginaSEI::getInstance()->getProxTabDados() ?>" onclick="adicionarProcesso();" accesskey="A" id="btnAdicionar" class="infraButton" style="display: none"><span class="infraTeclaAtalho">A</span>dicionar</button>
     </div>
-    <!--FIM TIPO-->
+    <!-- FIM TIPO DO PROCESSO VALIDADO -->
 <div class="clear"></div>
-    <!-- GRID DE PROCESSO -->
+    <!-- INICIO GRID DO PROCESSO ADICIONADO -->
     <div class="bloco" style="width: 100%;">
 
         <table width="99%" class="infraTable" summary="Demanda" id="tbProcesso" style="display: none;">
@@ -47,7 +32,7 @@
             </tr>
 
         </table>
-        <!--FIM TABELA Processo-->
+        <!-- FIM GRID DO PROCESSO ADICIONADO -->
         <input type="hidden" name="hdnIdTipoProcedimento" id="hdnIdTipoProcedimento" value=""/>
         <input type="hidden" name="hdnTbProcesso" id="hdnTbProcesso" value="<?php echo $strGridProcesso?>"/>
         <input type="hidden" name="hdnProcessoIntercorrente" id="hdnProcessoIntercorrente" value=""/>

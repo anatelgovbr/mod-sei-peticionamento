7b25f7b72be94721188134d5a4d7dd3af625c84a
Nei Jobson <neijobson@anatel.gov.br>
2019-11-08 12:21:17 -0300

    [3.0.0] Correção no botão Peticionamento Intercorrente a partir do Acesso Externo de um processo - PeticionamentoIntegracao

--- sei/web/modulos/peticionamento/PeticionamentoIntegracao.php
+++ sei/web/modulos/peticionamento/PeticionamentoIntegracao.php
@@ -1143,7 +1143,7 @@ class PeticionamentoIntegracao extends SeiIntegracao
             $urlBase = ConfiguracaoSEI::getInstance()->getValor('SEI', 'URL');
 
             $link = $urlBase . '/controlador_externo.php?acao=md_pet_intercorrente_usu_ext_cadastrar&id_orgao_acesso_externo=0&infra_hash=' . $hash;
-            $id_procedimento = $_GET['id_procedimento'];
+            $id_procedimento = isset($_GET['id_procedimento']) ? $_GET['id_procedimento'] : $objProcedimentoAPI->getIdProcedimento();
 
             $array[] = "<script> function criarForm(){ 
             var f = document.createElement(\"form\");

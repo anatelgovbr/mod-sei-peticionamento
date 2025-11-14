072dfb1b0d3cc114c2297e99839eb6ee35957a7b
gabrielg.colab <gabrielg.colab@anatel.gov.br>
2025-11-14 11:08:57 -0300

    Subindo versão para 4.3.5

--- sei/web/modulos/peticionamento/PeticionamentoIntegracao.php
+++ sei/web/modulos/peticionamento/PeticionamentoIntegracao.php
@@ -32,7 +32,7 @@ class PeticionamentoIntegracao extends SeiIntegracao
 
     public function getVersao()
     {
-        return '4.3.4';
+        return '4.3.5';
     }
 
     public function getIaMenorVersaoRequerida()
